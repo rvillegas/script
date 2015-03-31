78193f081b1c86b806614a9bc1eb898f289bc95f
rvillegas <ramirovillegasisaza@gmail.com>
2015-03-31 12:13:51 -0500

    Mar31_2015
    Actualizar gitignore, backups

--- bu.bat
+++ bu.bat
@@ -1,4 +1,4 @@
 sqlcmd -U taller -P taller -S SISTEMAS-PC\SQLEXPRESS -d dbST -Q "DBCC SHRINKDATABASE(N'dbST' )"
-
+sqlcmd -U taller -P taller -S SISTEMAS-PC\SQLEXPRESS -i D:\datos\script\backup_dbs.sql
 xcopy d:\datos\*.* \\Taller_maquipos\!BU\datos_srv\ /s /y
 set /p DUMMY=Hit ENTER to continue...
